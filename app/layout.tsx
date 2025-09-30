bb5d2a4c3f7ac7a2cdfa8cb6456863021fd7deac
SultanMukhtar <sultan.khudori@binus.ac.id>
2025-09-30 20:59:18 +0700

    third commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,25 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { GeistSans } from "geist/font/sans"
-import { GeistMono } from "geist/font/mono"
-import { Analytics } from "@vercel/analytics/next"
-import { Suspense } from "react"
-import "./globals.css"
+import type React from "react";
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import { Analytics } from "@vercel/analytics/next";
+import { Suspense } from "react";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.app",
-}
+  title: "Sultan Mukhtar Khudori | Portfolio",
+  description:
+    "Software Engineering student at BINUS University. Passionate about web development, AI, and product development.",
+  generator: "Next.js",
+  icons: {
+    icon: "/favicon.ico", // letakkan favicon.ico di folder /public
+  },
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en" className="scroll-smooth antialiased">
@@ -24,5 +28,5 @@ export default function RootLayout({
         <Analytics />
       </body>
     </html>
-  )
+  );
 }
